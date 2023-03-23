2deff0e4fc28150ae2c0cb6572d7de5d1c1afd31
genioustar <4574gkdl>
2023-03-23 17:33:57 +0900

    vanillajs에서 redux활용한것 마무리!

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,75 @@
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+import { createStore } from "redux";
 
-let count = 0;
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-if (number) {
-  number.innerHTML = count.toString();
-}
+const ADD_TODO = "ADD_TODO";
+const DEL_TODO = "DEL_TODO";
 
-// 이걸 매 클릭마다 넣어줘야하는 이유는! 데이터는 변했지만 변한 데이터를 html에다가 변한값을 넣어줘야 하기 때문에!
-const updateText = () => {
-  if (number) {
-    number.innerHTML = count.toString();
+// action만 리턴하는 함수들을 reducer 위에 다가 정의한다.(일반적으로)
+const addToDo = (text: string) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteToDo = (id: number) => {
+  return {
+    type: DEL_TODO,
+    id,
+  };
+};
+
+const reducer = (state: any = [], action: any) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: state.length + 1 }]; // new array를 만들어서 return해야함!!! state.push(action.text)하면 안됨!
+    case DEL_TODO:
+      return state.filter((toDo: any) => toDo.id !== action.id);
+    default:
+      return state;
   }
 };
-const handleAdd = () => {
-  console.log("++++");
-  count = count + 1;
-  updateText();
+
+const store = createStore(reducer);
+const dispatchAddToDo = (text: string) => {
+  store.dispatch(addToDo(text));
+};
+const delToDo = (e: any) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
 };
 
-const handleMinus = () => {
-  console.log("----");
-  count = count - 1;
-  updateText();
+const paintToDos = () => {
+  if (ul) ul.innerHTML = "";
+  const toDos = store.getState();
+  toDos.forEach((toDo: any) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", delToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul?.appendChild(li);
+  });
 };
 
-add?.addEventListener("click", handleAdd);
-minus?.addEventListener("click", handleMinus);
-export default function Index() {
-  return <div></div>;
-}
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.subscribe(paintToDos);
+
+const onSubmit = (e: any) => {
+  e.preventDefault();
+  if (input) {
+    const toDo = input.value;
+    input.value = "";
+    dispatchAddToDo(toDo);
+  }
+};
+
+form?.addEventListener("submit", onSubmit);
