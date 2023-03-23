14bbe44134b88adf59a4d8b081e5ef8f1dab3f58
genioustar <4574gkdl>
2023-03-23 18:05:16 +0900

    진정 ts형태로 전환

--- src/index.tsx
+++ src/index.tsx
@@ -1,56 +1,76 @@
 import { createStore } from "redux";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+const form = document.querySelector("form") as HTMLFormElement;
+const input = document.querySelector("input") as HTMLInputElement;
+const ul = document.querySelector("ul") as HTMLUListElement;
 
+// 사용자 실수를 방지하기 위한 변수로 선언!
 const ADD_TODO = "ADD_TODO";
 const DEL_TODO = "DEL_TODO";
 
-// action만 리턴하는 함수들을 reducer 위에 다가 정의한다.(일반적으로)
-const addToDo = (text: string) => {
+interface Todo {
+  text: string;
+  id: number;
+}
+
+interface AddTodoAction {
+  type: typeof ADD_TODO;
+  text: string;
+}
+
+interface DeleteTodoAction {
+  type: typeof DEL_TODO;
+  id: number;
+}
+
+type TodoActionTypes = AddTodoAction | DeleteTodoAction;
+
+const addToDo = (text: string): AddTodoAction => {
   return {
     type: ADD_TODO,
     text,
   };
 };
 
-const deleteToDo = (id: number) => {
+const deleteToDo = (id: number): DeleteTodoAction => {
   return {
     type: DEL_TODO,
     id,
   };
 };
 
-const reducer = (state: any = [], action: any) => {
+const reducer = (state: Todo[] = [], action: TodoActionTypes): Todo[] => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: state.length + 1 }]; // new array를 만들어서 return해야함!!! state.push(action.text)하면 안됨!
+      return [...state, { text: action.text, id: state.length + 1 }];
     case DEL_TODO:
-      return state.filter((toDo: any) => toDo.id !== action.id);
+      return state.filter((toDo: Todo) => toDo.id !== action.id);
     default:
       return state;
   }
 };
 
 const store = createStore(reducer);
+
 const dispatchAddToDo = (text: string) => {
   store.dispatch(addToDo(text));
 };
+
 const delToDo = (e: any) => {
-  const id = parseInt(e.target.parentNode.id);
+  console.dir(e);
+  const id = parseInt(e.target.parentNode!.id);
   store.dispatch(deleteToDo(id));
 };
 
 const paintToDos = () => {
   if (ul) ul.innerHTML = "";
   const toDos = store.getState();
-  toDos.forEach((toDo: any) => {
+  toDos.forEach((toDo: Todo) => {
     const li = document.createElement("li");
     const btn = document.createElement("button");
     btn.innerText = "DEL";
     btn.addEventListener("click", delToDo);
-    li.id = toDo.id;
+    li.id = toDo.id.toString();
     li.innerText = toDo.text;
     li.appendChild(btn);
     ul?.appendChild(li);
@@ -63,7 +83,7 @@ store.subscribe(() => {
 
 store.subscribe(paintToDos);
 
-const onSubmit = (e: any) => {
+const onSubmit = (e: Event) => {
   e.preventDefault();
   if (input) {
     const toDo = input.value;
